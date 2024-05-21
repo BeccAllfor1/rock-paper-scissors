f36f01fb9523039cc1c9035a3d025f4945c711f7
Rebecca Allford <rebeccaallford1@gmail.com>
2024-05-21 15:51:31 +0000

    added html and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,31 @@
 
+//document.addEventListener("DOMContentLoaded"), () => {
+
+const choices = ["rock", "paper", "scissors","lizard", "spock" ];
+let playerChoice = " ";
+let resultScreen = ("section-picked");
+
+function startGame() {
+    homeScreen.classList.add("hidden");
+    resultScreen.classList.add("hidden");
+    gameScreen.ClassList.remove("hidden");
+
+    startGame();
+}
+
+
+
+
+
+
+
 
 
 /**
- * Gets the current score from the DOM and increments by 1
- */
+* Gets the current score from the DOM and increments by 1
+*/
 
-function incrementScore() {
-    let oldScore = parseInt(document.getElementById("score").innerText);
-    document.getElementById("score").innerText = ++oldScore;
-}
+//function incrementScore() {
+//    let oldScore = parseInt(document.getElementById("score").innerText);
+//   document.getElementById("score").innerText = ++oldScore;
+//}
